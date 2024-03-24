e06af2dc859a630587cba47b3de636458af49cab
frontendrocks <gp9141@gmail.com>
2024-03-25 01:53:45 +0530

    change i config file

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
 export const PORT = 5001;
-export const mongoDBURL = 'mongodb+srv://<userName>:<passWord>@product-store-mern.t3agqzh.mongodb.net/?retryWrites=true&w=majority&appName=product-store-mern';
+export const mongoDBURL = 'mongodb+srv://<user>:<password>@product-store-mern.t3agqzh.mongodb.net/?retryWrites=true&w=majority&appName=product-store-mern';
